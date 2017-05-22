0c7a8cb8fde6ab29c54d495ad0465078c9d496f0
Siegi96 <michael.sieghartsleitner@outlook.com>
2017-05-22 18:01:32 +0200

    News

--- js/content_code.js
+++ js/content_code.js
@@ -2,10 +2,24 @@
  * Created by Siegi on 22.05.2017.
  */
 
-var showSearchBar = "true"
+var SearchBarShowed = false;
+var searchTriangle = $("#searchTriangle");
+var searchBarDesktop = $("#searchBarDesktop");
 
-$("#searchTriangle").on( "click", showSearchBar );
+searchTriangle.on( "click", showSearchBar );
 function showSearchBar() {
-    alert("hasdf");
-    $("#searchBarDesktop").css("display", "inherit");
+    if(!SearchBarShowed){
+        searchBarDesktop.css("opacity", "1");
+        searchBarDesktop.css("margin-bottom", "65");
+        searchTriangle.toggleClass("fa-search-plus fa-search-minus");
+
+
+        SearchBarShowed=true;
+    }
+    else{
+        searchBarDesktop.css("opacity", "0");
+        searchBarDesktop.css("margin-bottom", "0");
+        searchTriangle.toggleClass("fa-search-minus fa-search-plus");
+        SearchBarShowed=false;
+    }
 }
