3b2356cc360107a88b7a74d2ddd460e8b2212c7b
Siegi96 <michael.sieghartsleitner@outlook.com>
2017-05-22 15:10:46 +0200

    Kofigurator

---
+++ js/content_code.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Siegi on 22.05.2017.
+ */
+
+var showSearchBar = "true"
+
+$("#searchTriangle").on( "click", showSearchBar );
+function showSearchBar() {
+    alert("hasdf");
+    $("#searchBarDesktop").css("display", "inherit");
+}
