7ab929fc3ff20ac0767415937f05b08b3689486d
Siegi96 <michael.sieghartsleitner@outlook.com>
2017-05-23 08:25:10 +0200

    Kleinigkeiten

--- js/content_code.js
+++ js/content_code.js
@@ -23,3 +23,27 @@ function showSearchBar() {
         SearchBarShowed=false;
     }
 }
+
+
+var SearchBarShowedMobile = false;
+var mobilFilterButton = $("#mobileFilterButton");
+var mobilefilter = $("#mobilefilter");
+
+mobilFilterButton.on( "click", showMobileSearchBar );
+function showMobileSearchBar() {
+    if(!SearchBarShowedMobile){
+        mobilefilter.css("display", "inherit");
+        mobilefilter.css("margin-bottom", "65");
+        $("#filterArrow").toggleClass("fa-arrow-down fa-arrow-up");
+
+
+        SearchBarShowedMobile=true;
+    }
+    else{
+        mobilefilter.css("display", "none");
+        mobilefilter.css("margin-bottom", "0");
+        $("#filterArrow").toggleClass("fa-arrow-up fa-arrow-down");
+        SearchBarShowedMobile=false;
+    }
+}
+
